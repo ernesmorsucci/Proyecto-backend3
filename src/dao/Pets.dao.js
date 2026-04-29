7317d8b3fb97da154dae7a00f834aea551b21427
ErnesDev <ernesmorsucci14@gmail.com>
2026-04-29 11:40:49 -0300

    adoptme dao

---
+++ src/dao/Pets.dao.js
@@ -0,0 +1,20 @@
+import petModel from "./models/Pet.model.js";
+
+export default class Pets{
+
+    get=(params)=>{
+        return petModel.find(params);
+    }
+    getBy=(params)=>{
+        return petModel.findOne(params);
+    }
+    save=(doc)=>{
+        return petModel.create(doc);
+    }
+    update=(id,doc)=>{
+        return petModel.findByIdAndUpdate(id,{$set:doc});
+    }
+    delete=(id)=>{
+        return petModel.findByIdAndDelete(id);
+    }
+}
